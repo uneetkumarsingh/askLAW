ed78424ac52fae26db6a55153cb71e07748f25a3
uneetsingh <uneet.singh1@cactusglobal.com>
2023-11-11 02:07:17 +0530

    Streamplit completed

--- app/asklaw.py
+++ app/asklaw.py
@@ -38,12 +38,10 @@ if st.button('Answer'):
                 st.write(f'URL: {url}')
 
 
-# latest_iteration = st.empty()
-# bar = st.progress(0)
-
-# async 
-# for i in range(10):
-#   # Update the progress bar with each iteration.
-#   latest_iteration.text(f'Iteration {i+1}')
-#   bar.progress(i + 1)
-#   time.sleep(0.1)
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
